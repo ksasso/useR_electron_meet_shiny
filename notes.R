cd77070c5e24773f344f1bb3070cc335e6a9ed6a
Katie Sasso <ksasso@MBP2018-007.local>
2018-06-14 12:31:27 -0400

    initial commit

---
+++ notes.R
@@ -0,0 +1,12 @@
+Electron is a chromium web browswer and a nodeJS runtime brought together in a beautiful harmony to allow us to build client applications on any platform using web technologies. 
+
+Can use this as a window to shiny applications 
+Instance of a browser window in electron 
+
+
+Navigate to our Sharepoint folder, here , to find the "ShinyApp.zip" file you will need. Download the ShinyApp.zip file and unzip. Let us know if you have problems accessing it. 
+
+Once the file has been unzipped, just follow these steps to launch the test app:
+  1.	Within the main "ShinyApp" folder, navigate to the following subdirectory: Build --> ShinyApp
+2.	Double click on the "shinyApp" application file (highlighted in attached screenshot)
+3.	Make sure you can see and interact with the shiny app as depicted in the second attachment. If you can, we have success! If you can't, open up task manager and make sure you only have one instance of the ShinyApp electron running. End tasks on them all if you have more than 1 and re try. 
